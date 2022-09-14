fb3de1cdc6ed4c5f36b8987b1131d4bd73fc984a
piotr-mrocz <p.mrocz@vp.pl>
2022-09-14 18:54:54 +0200

    Recreate project and add tailwind css

--- AngularChangeLanguage/karma.conf.js
+++ AngularChangeLanguage/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-change-language'),
+      dir: require('path').join(__dirname, './coverage/tailwind-css-information'),
       subdir: '.',
       reporters: [
         { type: 'html' },
