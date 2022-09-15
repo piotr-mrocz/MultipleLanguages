f153329082d8320d70b06c8e57985f99d8e3a68c
piotr-mrocz <p.mrocz@vp.pl>
2022-09-15 21:01:21 +0200

    Dynamically change language on page

--- AngularChangeLanguage/src/app/app.module.ts
+++ AngularChangeLanguage/src/app/app.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/languages/', '.json');
+}
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularChangeLanguage/src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('pl');
+    translate.use('pl');
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+
+  ngOnInit(): void {
+    this.translate.use('pl');
+  }
+}
